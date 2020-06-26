f5c44c660a68a6e06a31b467e98327a20e0896e0
Scott Griffin <mscottgriffin@gmail.com>
2020-06-26 10:38:37 -0400

    added psuedocode to index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+//require fs
+// require inquierer
+
 // array of questions for user
 const questions = [
+ // see documentaion on npmjs
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  //us the fs package
 }
 
 // function to initialize program
 function init() {
-
+// use the inquierer package
 }
 
 // function call to initialize program
